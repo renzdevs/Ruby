bc69b5f1bb21c64da58f5930a7af7f6ab2f825d0
madoon <RaineLarbi80@hotmail.com>
2026-02-25 17:07:26 -0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,8 +1,1 @@
-"""
-hardware/
-FlexBone-X locomotion controller + PurrSynth audio interface.
-"""
-from .locomotion import LocomotionController
-from .audio import PurrSynth
-
-__all__ = ["LocomotionController", "PurrSynth"]
+# See ruby/firmware/ for the Python package
